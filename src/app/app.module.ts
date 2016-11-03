960a1b26f431190b29f85c399a19abd597c4d42f
Victor Riempp <vriempp@conet.de>
2016-11-03 14:14:47 +0100

    added component book-list and added a button that shows a hidden text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
