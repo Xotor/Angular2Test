23e145bff01b01caae5c89912f4c7477b9767520
Victor Riempp <vriempp@conet.de>
2016-11-03 14:43:32 +0100

    added box tracking the mouse movement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   isHidden = true;
+  isDisabled = false;
+  x: number;
+  y: number;
+  onMousemove($event:MouseEvent){
+    this.x=$event.clientX;
+    this.y=$event.clientY;
+  }
 }
